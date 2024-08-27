31e29d786fa3840a7739853eb0e7f0bf573064ea
Joseph Wangai <wangaijoseph4@gmail.com>
2024-08-27 18:25:31 +0300

    f

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import { useRecipeStore } from './recipeStore';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom'; // Import Link for navigation
+import useRecipeStore from '../recipeStore';
 
 const RecipeList = () => {
-    const recipes = useRecipeStore((state) => state.filteredRecipes);
-    const filterRecipes = useRecipeStore((state) => state.filterRecipes);
-  
-    useEffect(() => {
-      filterRecipes();
-    }, [filterRecipes]);
-  
-    return (
-      <div>
-        {recipes.map((recipe) => (
-          <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
-            <p>{recipe.description}</p>
-          </div>
-        ))}
-      </div>
-    );
-  };
-  
-  export default RecipeList;
-  
+  const recipes = useRecipeStore((state) => state.filteredRecipes);
+  const filterRecipes = useRecipeStore((state) => state.filterRecipes);
+
+  useEffect(() => {
+    filterRecipes();
+  }, [filterRecipes]);
+
+  return (
+    <div>
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h3>
+            <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link>
+          </h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecipeList;
